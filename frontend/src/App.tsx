f541990a83f5dc8c9e005b07881cd39e1031ce3b
RadonZ <supachai27102000@gmail.com>
2024-08-08 15:49:13 +0700

    add: router connect to dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import AppLayout from "./components/Layout/AppLayout";
+import Dashboard from "./screen/Dashboard";
+import GlobalStyles from "./styles/GlobalStyles";
 
 
 export default function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+    <GlobalStyles />
+    <BrowserRouter>
+    <Routes>
+      <Route element={<AppLayout/>}>
+      <Route index element={<Navigate replace to="dashboard"/>}/>
+      <Route path="dashboard" element={<Dashboard/>}/>
+
+      </Route>
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
