6ea5e41d4820ac79640d1a1690b49cef47795a6a
RadonZ <supachai27102000@gmail.com>
2024-08-27 16:09:55 +0700

    add: new navigate

--- frontend/src/components/ui/MainNav.tsx
+++ frontend/src/components/ui/MainNav.tsx
@@ -1,35 +1,81 @@
-import React from 'react'
+
 import styled from 'styled-components'
+import NavListItem from './NavListItem'
+import { FaAngleDown, FaAngleUp, FaBook, FaCalendar, FaHandHoldingHeart, FaHome, FaQuestionCircle } from 'react-icons/fa'
+import { HiDocument, HiInbox } from 'react-icons/hi2'
 
 const NavList = styled.ul`
-    display: flex
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
 `
 
 const NAV_LIST_DATA = [
     {
         to:"/service",
-        label: "Service"
+        label: "Service",
+        icon: <FaHandHoldingHeart />
     },
     {
         to: "/dashboard",
-        label: "Dasnboard"
+        label: "Dasnboard",
+        icon: <FaHome />
     },
     {
-        to: "/"
+        to: "/calendar",
+        icon: <FaCalendar/>,
+        label: "Calendar"
+    },
+    {
+        to: "/reservation",
+      label: "Reservation",
+      icon:  <HiInbox />,
+      iconClosed:  <FaAngleDown />,
+      iconOpened: <FaAngleUp />,
+      subNav: [
+        {
+            to: "/classroom",
+            label: "Classroom"
+        },
+        {
+            to: "/meeting",
+            label: "Meeting"
+        }
+      ]
+    },
+    {
+      to: "/tracking",
+      label: "Tracking",
+      icon:  <HiInbox />,
+      iconClosed:  <FaAngleDown />,
+      iconOpened: <FaAngleUp />,
+      subNav: [
+        {
+            to: "/form",
+            label: "Form"
+        },
+        {
+            to: "/reservation/mailandparcel",
+            label: "Mail/Parcel"
+        }
+      ]
     }
 ]
 
 const NAV_LIST_HELP = [
     {
         to: "/document",
+        icon: <HiDocument />,
         label: "Document"
     },
     {
-        to: "/knowledge-base",
+        to: "/knowledgebase",
+        icon: <FaBook />,
         label: "Knowledge Base"
     },
     {
-        to: "/Support",
+        to: "/support",
+        icon:<FaQuestionCircle />,
         label: "Support"
     }
 ]
@@ -38,7 +84,26 @@ export default function MainNav() {
   return (
     <nav>
         <NavList>
-
+            {NAV_LIST_DATA.map((item) => (
+                <NavListItem 
+                key={item.to} 
+                to={item.to}
+                icon={item.icon} 
+                label={item.label}
+                subNav = {item.subNav}
+                iconClosed={item.iconClosed}
+                iconOpened={item.iconOpened}
+                />
+            ))}
+            <h3>Help</h3>
+            {NAV_LIST_HELP.map((item) => (
+                <NavListItem
+                    key={item.to}
+                    icon={item.icon}
+                    to={item.to}
+                    label={item.label}
+                    subNav={undefined} iconClosed={undefined} iconOpened={undefined}                />
+            ))}
         </NavList>
     </nav>
   )
