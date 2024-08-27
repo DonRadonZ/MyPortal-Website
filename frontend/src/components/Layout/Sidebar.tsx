f6ede49cac9ccb7359d2eccb4203ce56e042f033
RadonZ <supachai27102000@gmail.com>
2024-08-27 16:09:09 +0700

    update: color and design to sidebar

--- frontend/src/components/Layout/Sidebar.tsx
+++ frontend/src/components/Layout/Sidebar.tsx
@@ -1,21 +1,23 @@
 import styled from "styled-components"
+import MainNav from "../ui/MainNav"
 
 const StyledSidebar = styled.aside`
-  background-color: #fff;
-    padding: 3.2rem 2.4rem;
+  background-color: var(--color-gray-0);
+    padding: 3.25rem 2.5rem;
     
-    border-right: 1px solid #f3f4f6;
+    border-right: 1px solid var(--color-gray-100);
     grid-row: 1 / -1;
     display: flex;
     flex-direction: column;
-    gap: 3.2rem;
+    gap: 2.5rem;
+    overflow-y: auto;
 
 `
 
 export default function Sidebar() {
   return (
     <StyledSidebar>
-      Sidebar
+      <MainNav/>
     </StyledSidebar>
   )
 }
