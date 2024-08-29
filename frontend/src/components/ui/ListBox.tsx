3b37ac79a8a9826f1839ebafcad1b2b7e0827943
RadonZ <supachai27102000@gmail.com>
2024-08-29 16:16:19 +0700

    add: new components for page

---
+++ frontend/src/components/ui/ListBox.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledBox = styled.div`
+    background-color: var(--color-gray-0);
+    border: 1px solid var(--color-gray-100);
+    border-radius: var(--border-radius-md);
+
+`
+
+export default StyledBox
