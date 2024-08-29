f8243fd4799d7c37f5224c8ee92381083abba3e3
RadonZ <supachai27102000@gmail.com>
2024-08-29 16:19:14 +0700

    add: pagination for change page

---
+++ frontend/src/components/utils/Pagination.tsx
@@ -0,0 +1,14 @@
+
+import styled from 'styled-components'
+
+const StyledPagination = styled.div`
+
+`
+
+export default function Pagination() {
+  return (
+    <StyledPagination>
+        Pagination
+    </StyledPagination>
+  )
+}
