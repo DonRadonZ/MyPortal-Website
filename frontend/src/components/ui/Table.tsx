f717ee032919cd20f613f244d9e0d90a24e223bc
RadonZ <supachai27102000@gmail.com>
2024-08-30 16:34:52 +0700

    update: color table

--- frontend/src/components/ui/Table.tsx
+++ frontend/src/components/ui/Table.tsx
@@ -24,7 +24,7 @@ const CommonRow = styled.div<CommonRowProps>`
 
 const StyledHeader = styled(CommonRow)`
     padding: 1.6rem 2.4rem;
-    background-color: var(--color-gray-50);
+    background-color: var(--color-gray-0);
     border-bottom: 1px solid var(--color-gray-100);
     text-transform: uppercase;
     letter-spacing: 0.4px;
