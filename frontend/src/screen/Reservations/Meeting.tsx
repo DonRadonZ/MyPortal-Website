48ed1a89a7a43db9d9245ce4263354689c375607
RadonZ <supachai27102000@gmail.com>
2024-08-30 16:39:40 +0700

    add: table for data

--- frontend/src/screen/Reservations/Meeting.tsx
+++ frontend/src/screen/Reservations/Meeting.tsx
@@ -1,8 +1,12 @@
 import Heading from "../../components/Typography/Heading";
+import MeetingTable from "../../feature/meeting/MeetingTable";
 
 
 export default function Meeting() {
   return (
+    <>
     <Heading>Meeting Reservation</Heading>
+    <MeetingTable/>
+    </>
   )
 }
--- frontend/src/screen/Tracking/Form.tsx
+++ frontend/src/screen/Tracking/Form.tsx
@@ -1,10 +1,13 @@
 import Heading from "../../components/Typography/Heading";
+import FormTable from "../../feature/form/FormTable";
+
 
 
 export default function Form() {
   return (
     <>
         <Heading as="h1">Form Tracking</Heading>
+        <FormTable/>
     </>
   )
 }
