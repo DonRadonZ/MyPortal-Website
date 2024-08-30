a8f020aac9b723daf70301e75200242a65a479ac
RadonZ <supachai27102000@gmail.com>
2024-08-30 16:34:09 +0700

    Add: search part

---
+++ frontend/src/components/Form/Search.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Search = styled.input`
+    border: 1px solid var(--color-gray-300);
+    background-color: var(--color-gray-0);
+    border-radius: var(--border-radius-sm);
+    padding: 0.8rem 2.4rem;
+    width: 40rem;
+    box-shadow: var(--shadow-sm);
+`;
+
+export default Search;
