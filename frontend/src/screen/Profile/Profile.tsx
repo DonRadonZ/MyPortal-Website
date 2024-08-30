5c996d1df769ca0771ab05b1b20477045ca35e83
RadonZ <supachai27102000@gmail.com>
2024-08-30 16:39:28 +0700

    add: profile design part

--- frontend/src/screen/Profile/Profile.tsx
+++ frontend/src/screen/Profile/Profile.tsx
@@ -1,10 +1,16 @@
+import Row from "../../components/layout/Row";
 import Heading from "../../components/Typography/Heading";
+import ProfileViewOperation from "../../feature/profile/ProfileViewOperation";
+
 
 
 export default function Profile() {
   return (
     <>
+    <Row type="horizontal">
     <Heading>Profile</Heading>
+    <ProfileViewOperation/>
+    </Row>
     </>
   )
 }
