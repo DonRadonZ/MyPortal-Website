27791e7ecfd16c32c342cd402b89763ed33026eb
RadonZ <supachai27102000@gmail.com>
2024-08-30 16:37:42 +0700

    add: table

--- frontend/src/screen/Reservations/Classroom.tsx
+++ frontend/src/screen/Reservations/Classroom.tsx
@@ -1,8 +1,12 @@
 import Heading from "../../components/Typography/Heading";
+import ClassTable from "../../feature/classroom/ClassTable";
 
 
 export default function Classroom() {
   return (
+    <>
     <Heading>Classroom Reservation</Heading>
+    <ClassTable/>
+    </>
   )
 };
