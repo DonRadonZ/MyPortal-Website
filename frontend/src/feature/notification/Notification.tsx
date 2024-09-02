35b2fe19fa1e474733107a009696d4b02ac48d71
RadonZ <supachai27102000@gmail.com>
2024-09-02 10:25:40 +0700

    add: notification part

--- frontend/src/feature/notification/Notification.tsx
+++ frontend/src/feature/notification/Notification.tsx
@@ -1,7 +1,14 @@
+import { useState } from 'react'
 import ButtonIcon from '../../components/ui/ButtonIcon'
 import { FaBell } from 'react-icons/fa'
 
 export default function Notification() {
+  const [showNotifactions, setShowNotifications] = useState(false);
+
+  function toggleNotification(){
+    setShowNotifications(!showNotifactions);
+  }
+
   return (
     <ButtonIcon>
         <FaBell />
