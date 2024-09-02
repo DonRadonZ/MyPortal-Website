6ae7f5367f38c9e0aea686d421d8ddf0b5bc3b2b
RadonZ <supachai27102000@gmail.com>
2024-09-02 16:44:04 +0700

    add: custom hook outside click

---
+++ frontend/src/hooks/useOutsideClick.ts
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react';
+
+function useOutsideClick(handler: any, listenerCapturing = true) {
+    const ref: any = useRef();
+
+    useEffect(function(){
+        function handleClick(e: Event) {
+            if (ref.current && !ref.current.contains(e.target)){ handler(); 
+
+            }
+        }
+        
+        document.addEventListener('click', handleClick, true);
+
+        return () => document.removeEventListener("click", handleClick, listenerCapturing)
+    }, [handler, listenerCapturing]
+);
+
+    return ref;
+}
+
+export default useOutsideClick
