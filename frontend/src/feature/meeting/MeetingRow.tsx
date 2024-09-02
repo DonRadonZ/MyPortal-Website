e2c4bc8681452d0b9fb6067395f31df179057a6c
RadonZ <supachai27102000@gmail.com>
2024-09-02 16:48:46 +0700

    add: data to table

--- frontend/src/feature/meeting/MeetingRow.tsx
+++ frontend/src/feature/meeting/MeetingRow.tsx
@@ -11,17 +11,46 @@ const Title = styled.div`
 
 type MeetingRowProps = {
     meeting:{
-        id: number;
-    created_at: string;
-    title: string;
-    room: string;
-    booker: string;
-    date: string;
-    status: string;}
+        id: number,
+      title: string,
+      room: string,
+      booker: string,
+      dateTime: string,
+      timeStamp: string,
+      banquet: string,
+      status: string}
 }
 
-export default function MeetingRow({meeting}: MeetingRowProps) {
+export default function MeetingRow({meeting:{
+    // id: reservationId,
+    timeStamp: created_at,
+    title,
+    room,
+    booker,
+    dateTime,
+    banquet,
+    status
+}}: MeetingRowProps) {
   return (
-    <div>MeetingRow</div>
+    <Table.Row>
+        <Title>{title}</Title>
+        <span>{room}</span>
+        <span>{booker}</span>
+        <span>{dateTime}</span>
+        <span>{created_at}</span>
+        <span>{banquet}</span>
+        <span>{status}</span>
+        <div>
+            <button>
+                <HiEye/>
+            </button>
+            <button>
+                <HiPencil/>
+            </button>
+            <button>
+                <HiTrash/>
+            </button>
+        </div>
+    </Table.Row>
   )
 }
