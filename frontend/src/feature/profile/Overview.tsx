e75403a0d67088a0e8e45c7eaa6e27a30cbca559
RadonZ <supachai27102000@gmail.com>
2024-09-03 16:47:25 +0700

    add: new overview design

--- frontend/src/feature/profile/Overview.tsx
+++ frontend/src/feature/profile/Overview.tsx
@@ -1,7 +1,27 @@
+import styled from "styled-components"
+import Row from "../../components/layout/Row"
 
+const Box = styled.div`
+  background-color: var(--color-gray-0);
+  padding: 2.4rem 3.2rem;
+`
 
 export default function Overview() {
   return (
-    <div>Overview</div>
+    <Box>
+      <Row type="horizontal">
+        <div>
+          <h1>Guest Master</h1>
+          <h2>Software Engineer</h2>
+        </div>
+        <div>
+          Photo
+        </div>
+      </Row>
+      <h4>Major: Computer Center</h4>
+      <h4>Date-Birth: XX/XX/XXXX</h4>
+      <h4>Gender: XXXX</h4>
+      <h4>Staff ID: XXXXXXX</h4>
+    </Box>
   )
 }
